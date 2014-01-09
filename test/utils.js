d1ca29f7b653ceef9e97d9e6be0d84efbc879a44
Philmod <philmod@storify.com>
2014-01-08 18:51:11 -0800

    new line eof

--- test/utils.js
+++ test/utils.js
@@ -1,23 +1,23 @@
 module.exports = {
 
-  getKeys: function() {
-    var client_id = process.env.PLAID_CLIENTID
-    	, secret		= process.env.PLAID_SECRET
-    	;
+	getKeys: function() {
+		var client_id = process.env.PLAID_CLIENTID
+			, secret		= process.env.PLAID_SECRET
+			;
 
-    return {
-    		client_id : client_id
-    	, secret    : secret
-    };
-  },
+		return {
+				client_id : client_id
+			, secret    : secret
+		};
+	},
 
-  getUser: function() {
-    return {
-    		username : process.env.PLAID_USER_USERNAME
-    	, password : process.env.PLAID_USER_PASSWORD
-    	, type     : process.env.PLAID_USER_TYPE
-    	, email    : process.env.PLAID_USER_EMAIL
-    };
-  },
+	getUser: function() {
+		return {
+				username : process.env.PLAID_USER_USERNAME
+			, password : process.env.PLAID_USER_PASSWORD
+			, type     : process.env.PLAID_USER_TYPE
+			, email    : process.env.PLAID_USER_EMAIL
+		};
+	},
 
 }
