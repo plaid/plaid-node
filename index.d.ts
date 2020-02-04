69470368df4a9e7a483a0adf27ed868ace0c87cc
Skylar Brown <sbrown@plaid.com>
2020-02-03 17:30:05 -0800

    types

--- index.d.ts
+++ index.d.ts
@@ -631,6 +631,20 @@ declare module 'plaid' {
     webhook_fired: boolean;
   }
 
+  interface WebhookVerificationKeyResponse extends BaseResponse {
+    key: {
+      alg: string;
+      created_at: number;
+      crv: string;
+      expired_at: null | number;
+      kid: string;
+      kty: string;
+      use: string;
+      x: string;
+      y: string;
+    }
+  }
+
   interface ClientOptions extends CoreOptions {
     version?: '2019-05-29';
   }
@@ -951,6 +965,13 @@ declare module 'plaid' {
       access_token: string,
       webhook_code: string,
     ): Promise<SandboxItemFireWebhookResponse>;
+
+    // getWebhookVerificationKey(String, String)
+    getWebhookVerificationKey(
+      key_id: string,
+      cb: Callback<WebhookVerificationKeyResponse>,
+    ): Promise<WebhookVerificationKeyResponse>;
+
   }
 
   interface PlaidEnvironments {
