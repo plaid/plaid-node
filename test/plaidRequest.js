24a560452d32a67180c918f09184a0d4c5f4a0ac
Ivan <ivan@mish.guru>
2020-04-03 22:30:09 +1300

    chore: add test case for plaidRequest happy path

--- test/plaidRequest.js
+++ test/plaidRequest.js
@@ -12,23 +12,55 @@ const plaid = require('../');
 const plaidRequest = require('../lib/plaidRequest');
 
 const eq = assert.strictEqual;
+const deepEqual = assert.deepEqual;
 
 describe('plaid.plaidRequest', () => {
-  it('gracefully handles invalid JSON from the API', done => {
-      const scope = nock('https://sandbox.plaid.com');
-      nockingbird.load(scope, './test/mocks/api-invalid-json.nb');
+  const scope = nock('https://sandbox.plaid.com');
 
-      plaidRequest({
-        env: plaid.environments.sandbox,
-        public_key: 'xxx',
-      }, {
-        path: '/institutions/get_by_id',
-        body: {institution_id: 'ins_3'},
-      }, {}, (err, res) => {
-        eq(res, undefined);
-        eq(err.error_code, 'INTERNAL_SERVER_ERROR');
-        scope.done();
-        done();
-      });
+  it('gracefully handles invalid JSON from the API', done => {
+    nockingbird.load(scope, './test/mocks/api-invalid-json.nb');
+
+    plaidRequest({
+      env: plaid.environments.sandbox,
+      public_key: 'xxx',
+    }, {
+      path: '/institutions/get_by_id',
+      body: { institution_id: 'ins_3' },
+    }, {}, (err, res) => {
+      eq(res, undefined);
+      eq(err.error_code, 'INTERNAL_SERVER_ERROR');
+      scope.done();
+      done();
+    });
+  });
+
+  it('resolves with request_id and data on happy path (non-MFA)', done => {
+    nockingbird.load(scope, './test/mocks/api-valid-json.nb');
+
+    const expectedRes = {
+      request_id: '1234abcd',
+      buffer: {
+        // from response body
+        institution_id: 'ins_1',
+        description: 'more money, more problems',
+        // added by plaidRequest
+        status_code: 200
+      }
+    };
+
+    plaidRequest({
+      env: plaid.environments.sandbox,
+      public_key: 'yyy',
+    }, {
+      path: '/institutions/get_by_id',
+      body: { institution_id: 'ins_1' },
+    }, {}, (err, res) => {
+
+
+      deepEqual(res, expectedRes);
+      eq(err, null);
+      scope.done();
+      done();
+    });
   });
 });
